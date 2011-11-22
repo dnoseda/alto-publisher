a6b6d6af51ee5ad59c4807c61e908623f6d9fe23
dnoseda <damian.noseda@mercadolibre.com>
2011-11-21 21:50:35 -0300

    perfect

--- coder.c
+++ coder.c
@@ -1,33 +1,20 @@
-
-
-char e(char f) {
+char m(char f, int q){
     char j[256], u[261];
     int i;
     for(i=0; i<256;i++){int a =i+3; j[i]=a; u[a]=i; }
-    return j[f];
-}
-
-char d(char f) {
-    char j[256], u[261];
-    int i;
-    for(i=0; i<256;i++){int a =i+3; j[i]=a; u[a]=i; }
-    return u[f];
+    return q ? u[f] : j[f];
 }
 
 void gg(char *o, char *t, int w){
     FILE *i,*s;
-    s=fopen(t,"w");
-    i=fopen(o,"r");
+    s=fopen(t,"w");i=fopen(o,"r");
     char f;
     while(fscanf(i,"%c",&f) != EOF)
-        switch(w){
-            case 1: fprintf(s,"%c",f); break;
-            case 2: fprintf(s,"%c",e(f)); break;
-            case 3: fprintf(s,"%c",d(f)); break;
-        }
+    switch(w){case 1: fprintf(s,"%c",f); break;
+    case 2: fprintf(s,"%c",m(f,0)); break;
+    case 3: fprintf(s,"%c",m(f,1)); break; }
     
-    fclose(i);
-    fclose(s);
+    fclose(i);fclose(s);
 }
 
 void moddd(char *f, int w) {
